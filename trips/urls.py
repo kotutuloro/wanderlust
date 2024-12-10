89fda2b0bcfd895199efe15c6875b7cfcd3818cc
Kiko Otutuloro <kotutuloro@users.noreply.github.com>
2024-12-10 12:19:29 +0100

    Add project skeleton (#12)
    * install django
    
    * django-admin startproject
    
    * django startapp trips
    
    * remove admin site
    
    * add skeleton for trips routes

---
+++ trips/urls.py
@@ -0,0 +1,10 @@
+"""
+URL configuration for the trips app.
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
---
+++ trips/views.py
@@ -0,0 +1,13 @@
+"""
+Views for the trips app.
+"""
+
+from django.http import HttpResponse
+
+
+def index(_request):
+    """
+    TODO
+    """
+
+    return HttpResponse("Hello, world. You're at the trips index.")
