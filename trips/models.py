41de029dd51ace09233ac4e12ec8ca85fe86d712
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-12-10 18:05:20 +0100

    create models for Trips and Destinations

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+"""
+Models for the trips app.
+"""
 
-# Create your models here.
+from nanoid import generate as generate_nanoid
+from django.db import models
+from django.contrib.auth.models import User
+
+
+def generate_random_slug():
+    """Generates a 12 character nanoid"""
+    return generate_nanoid(size=12)
+
+
+class Trip(models.Model):
+    """Representation of the trip table"""
+
+    # primary key: id (auto set by django)
+    slug = models.SlugField(default=generate_random_slug,
+                            unique=True, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    scheduled = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)
+
+
+class Destination(models.Model):
+    """Representation of the destination table"""
+    # primary key: id (auto set by django)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
