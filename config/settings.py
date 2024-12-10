f5b830549fbe5f64ea7fd00e82ac69f502fb4d1d
Kiko Otutuloro <kotutuloro@users.noreply.github.com>
2024-12-10 18:56:07 +0100

    Add Trip and Destination models (#14)
    * use postgresql
    
    * create models for Trips and Destinations
    
    * create initial db migration for Trips and Destinations
    
    * add __str__ methods to models

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'trips.apps.TripsConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -74,8 +75,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wanderlust',
     }
 }
 
