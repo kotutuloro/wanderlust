ab9877aceedda65110b6b32f96aa4578f4f7147b
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-12-12 15:05:03 +0100

    use django auth signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,10 @@ URL configuration for the accounts app.
 """
 
 from django.urls import path, include
+from .views import SignUpView
 
 app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+"""
+Views for the accounts app
+"""
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class SignUpView(CreateView):
+    """
+    View for account registration.
+    """
+
+    form_class = UserCreationForm
+    success_url = reverse_lazy("accounts:login")
+    template_name = "registration/signup.html"
