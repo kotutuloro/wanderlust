c660c1d3cd85699b6cd58798704ff1641e86517b
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-12-12 15:30:11 +0100

    add account settings page

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,13 @@ URL configuration for the accounts app.
 """
 
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 from .views import SignUpView
 
 app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("settings/", TemplateView.as_view(template_name="registration/settings.html"), name="settings"),
+
 ]
