e1e81b2c9a6206b17449ca9507cefb856331f9fd
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-12-12 14:08:08 +0100

    startapp accounts

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+"""
+URL configuration for the accounts app.
+"""
+
+from django.urls import path, include
+
+app_name = "accounts"
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+]
