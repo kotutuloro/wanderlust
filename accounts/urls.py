53f29dec1d470fc3728837c16eb8f8d93039c5bb
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-01-24 11:36:25 +0100

    add django auth password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,16 @@
 URL configuration for the accounts app.
 """
 
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
 app_name = "accounts"
 urlpatterns = [
+    path("password_change/", auth_views.PasswordChangeView.as_view(
+        success_url=reverse_lazy("accounts:password_change_done"))),
     path("", include("django.contrib.auth.urls")),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("settings/", TemplateView.as_view(template_name="registration/settings.html"), name="settings"),
-
 ]
