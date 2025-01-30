3cbf72ea215875dbbb0ee15838acd7d6768812de
Kiko Otutuloro <kotutuloro@users.noreply.github.com>
2025-01-30 19:47:51 +0100

    Trips destinations (#19)
    * rough outline
    
    * rough sketch UserTripsView and template
    
    * TripDetails view and template
    
    * add error page templates
    
    * add CreateTrip view and template
    
    * add CreateDestination view and template
    
    * use specific trip in DestinationForm if given
    
    * list Destinations and show creation form in TripDetail
    
    * add path for new destination
    
    * change url names
    
    * add trip and dest forms to profile
    
    * use date and datetime inputs in forms

---
+++ trips/forms.py
@@ -0,0 +1,53 @@
+# accounts/forms.py
+from django.forms import ModelForm, ModelChoiceField, DateInput, DateTimeInput
+from .models import Trip, Destination
+
+
+class UIDateInput(DateInput):
+    input_type = "date"
+
+
+class UIDateTimeInput(DateTimeInput):
+    input_type = "datetime-local"
+
+
+class TripForm(ModelForm):
+    class Meta:
+        model = Trip
+        fields = ("title", "start_date", "end_date", "scheduled", "notes")
+        widgets = {
+            "start_date": UIDateInput(),
+            "end_date": UIDateInput(),
+        }
+
+
+class DestinationForm(ModelForm):
+    trip = ModelChoiceField(queryset=Trip.objects, required=False,
+                            empty_label="--- (Create a new trip for this destination) ---")
+
+    class Meta:
+        model = Destination
+        fields = ("trip", "name", "start_time", "end_time")
+        widgets = {
+            "start_time": UIDateTimeInput(),
+            "end_time": UIDateTimeInput(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        only_trip = kwargs.pop("only_trip", None)
+        self.user = kwargs.pop("user", None)
+        if not (only_trip or self.user):
+            raise Exception(
+                "DestinationForm requires either a user or a specific trip")
+        super().__init__(*args, **kwargs)
+
+        if only_trip:
+            self.fields['trip'].queryset = Trip.objects.filter(pk=only_trip.pk)
+            self.fields['trip'].initial = only_trip
+            self.fields['trip'].disabled = True
+        else:
+            self.fields['trip'].queryset = Trip.objects.filter(owner=self.user)
+
+    def clean_trip(self):
+        return (self.cleaned_data['trip']
+                or Trip.objects.create(owner=self.user, title=self.data.get('name')))
--- trips/views.py
+++ trips/views.py
@@ -2,7 +2,13 @@
 Views for the trips app.
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse
+
+from .models import Trip, Destination
+from .forms import TripForm, DestinationForm
 
 
 def index(request):
@@ -11,3 +17,74 @@ def index(request):
     """
 
     return render(request, "trips/index.html")
+
+
+class UserTripsView(LoginRequiredMixin, ListView):
+    """View for trips for a logged-in user."""
+    context_object_name = "user_trip_list"
+    template_name = "trips/profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["create_trip_form"] = TripForm()
+        context["create_dest_form"] = DestinationForm(user=self.request.user)
+        return context
+
+    def get_queryset(self):
+        return self.request.user.trip_set.all()
+
+
+class TripDetailView(UserPassesTestMixin, DetailView):
+    """View for single trip details."""
+    model = Trip
+    permission_denied_message = "You don't have access to this trip."
+
+    def test_func(self):
+        return self.request.user == self.get_object().owner
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["create_dest_form"] = DestinationForm(only_trip=self.object)
+        return context
+
+
+class CreateTripView(LoginRequiredMixin, CreateView):
+    """View for creating a new trip."""
+
+    template_name = "trips/create_trip.html"
+    form_class = TripForm
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class CreateDestinationView(UserPassesTestMixin, CreateView):
+    """View for creating a new destination."""
+
+    template_name = "trips/create_destination.html"
+    form_class = DestinationForm
+    permission_denied_message = "You don't have access to this trip."
+
+    def setup(self, request, *args, **kwargs):
+        self.trip = None
+        trip_slug = kwargs.get("trip_slug")
+        if trip_slug:
+            self.trip = get_object_or_404(Trip, slug=trip_slug)
+
+        return super().setup(request, *args, **kwargs)
+
+    def test_func(self):
+        if self.trip:
+            return self.request.user == self.trip.owner
+        return True
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        if self.trip:
+            kwargs["only_trip"] = self.trip
+        kwargs["user"] = self.request.user
+        return kwargs
+
+    def get_success_url(self):
+        return reverse("trips:trip-detail", args=[self.object.trip.slug])
