f1a75871b7308f7bb130275c62d39fd67f6e2cd8
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-02-12 13:47:35 +0100

    test UserTripsView

--- trips/tests/test_views.py
+++ trips/tests/test_views.py
@@ -12,16 +12,77 @@ class IndexViewTests(TestCase):
 
     def test_index_view_get(self):
         """
-        Test that the IndexView renders the index template.
+        The IndexView renders the index template.
         """
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "trips/index.html")
 
 
-class UserTripsViewTests(TestCase):
+class LoginRequiredTestMixin():
+    def test_not_logged_in(self):
+        """
+        A logged out user cannot access the path set as the instance's url.
+        """
+        self.client.logout()
+        response = self.client.get(self.url)
+        self.assertRedirects(
+            response, f"{reverse("accounts:login")}?next={self.url}")
+
+
+class UserTripsViewTests(LoginRequiredTestMixin, TestCase):
     def setUp(self):
         self.url = reverse("trips:profile")
+
+        self.user = User.objects.create(username="myuser", password="testpw")
+        self.client.force_login(self.user)
+
+    def test_shows_trips(self):
+        """
+        A user's trips are displayed.
+        """
+
+        trip1 = Trip.objects.create(owner=self.user, title="my cool trip")
+        trip2 = Trip.objects.create(owner=self.user, title="another trip")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "trips/profile.html")
+        self.assertQuerySetEqual(
+            response.context["user_trip_list"], [trip1, trip2], ordered=False)
+        self.assertContains(response, trip1.title)
+        self.assertContains(response, trip2.title)
+
+    def test_shows_no_trips(self):
+        """
+        No trips are shown if a user does not have any.
+        """
+        response = self.client.get(self.url)
+        self.assertQuerySetEqual(response.context["user_trip_list"], [])
+        self.assertContains(response, "No trips for you")
+
+    def test_shows_trips_only_for_logged_in_user(self):
+        """
+        Only trips owned by the logged in user are shown.
+        """
+        user_trip = Trip.objects.create(owner=self.user, title="my cool trip")
+        other_trip = Trip.objects.create(
+            owner=User.objects.create(), title="someone else's trip")
+
+        response = self.client.get(self.url)
+        self.assertQuerySetEqual(
+            response.context["user_trip_list"], [user_trip])
+        self.assertContains(response, user_trip.title)
+        self.assertNotContains(response, other_trip.title)
+
+    def test_includes_creation_forms(self):
+        """
+        Context includes forms for creating a trip and creating a destination.
+        """
+        response = self.client.get(self.url)
+        self.assertIsInstance(response.context["create_trip_form"], TripForm)
+        self.assertIsInstance(
+            response.context["create_dest_form"], DestinationForm)
 
 
 class TripDetailViewTests(TestCase):
