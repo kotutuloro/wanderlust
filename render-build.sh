5fb6c0c5bfd4818d846a2ae70a6fa1fb3de48f67
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-02-28 23:03:13 +0100

    add render build script

---
+++ render-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pipenv install
+
+export DJANGO_SETTINGS_MODULE="config.settings.production"
+
+# Convert static asset files
+python manage.py collectstatic -n --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
