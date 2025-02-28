c6517836dc91f04cba2b510b992d5d9d11fcea2c
Kikelomo Abiodun Otutuloro <k.otutuloro@gmail.com>
2025-02-28 22:41:55 +0100

    use dj-database-url

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,10 +1,19 @@
 from .base import *
 
+import dj_database_url
+
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
+}
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
